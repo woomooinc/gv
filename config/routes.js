1c3a936f2eec08948b956dfc62437f992562e9d5
ben <ben@dreamerslab.com>
2013-11-28 23:53:39 -0800

    add all routes

--- config/routes.js
+++ config/routes.js
@@ -1,3 +1,20 @@
 module.exports = function ( map ){
   map.get( '/','welcome#index' );
+
+  map.namespace( 'api', function ( api ){
+    api.get(  'login/facebook', 'fb#new' );
+    api.post( 'login/facebook', 'fb#create' );
+
+    api.get(  'players/:id', 'players#show' );
+    api.put(  'players/:id', 'players#update' );
+
+    api.get(  'games/:id', 'games#show' );
+    api.put(  'games',     'games#update_or_create' );
+
+    api.post( 'events', 'events#create' );
+
+    api.get(  'players/:player_id/events', 'player_events#show' );
+
+    api.get(  'socket', 'socket#watch' );
+  });
 };
