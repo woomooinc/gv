4c6f0e5a24b3674f4657e0e0cd01967599b66511
ben <ben@dreamerslab.com>
2013-11-29 09:28:28 -0800

    update routes

--- config/routes.js
+++ config/routes.js
@@ -5,16 +5,14 @@ module.exports = function ( map ){
     api.get(  'login/facebook', 'fb#new' );
     api.post( 'login/facebook', 'fb#create' );
 
-    api.get(  'players/:id', 'players#show' );
-    api.put(  'players/:id', 'players#update' );
-
-    api.get(  'games/:id', 'games#show' );
-    api.put(  'games',     'games#update_or_create' );
+    api.get(  'games/:id',       'games#show' );
+    api.put(  'games',           'games#update_or_create' );
+    api.put(  'games/:id',       'games#update' );
+    api.get(  'games/:id/watch', 'games#watch' );
 
     api.post( 'events', 'events#create' );
 
-    api.get(  'players/:player_id/events', 'player_events#show' );
+    api.get(  'histories/:id', 'histories#show' );
 
-    api.get(  'socket', 'socket#watch' );
   });
 };
