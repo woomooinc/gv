c77ba22a3c3a9cb5cc16bcc7a7fd43b8f36cf9ff
ben <ben@dreamerslab.com>
2013-11-29 21:26:10 -0800

    update controller and view for frontend

--- app/controllers/welcome.js
+++ app/controllers/welcome.js
@@ -3,6 +3,8 @@ var Class = require( 'node.class' );
 module.exports = Class.extend({
 
   index : function ( req, res, next ){
-    res.render( 'welcome/index' );
+    res.render( 'welcome/index', {
+      layout : false
+    });
   }
 });
